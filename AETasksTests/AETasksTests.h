fbcb78aa1a58b079243b0cdd1f225a66f3cd5835
Alex Esplin <alex.esplin@gmail.com>
2011-03-24 07:25:11 -0600

    Initial Commit

---
+++ AETasksTests/AETasksTests.h
@@ -0,0 +1,17 @@
+//
+//  AETasksTests.h
+//  AETasksTests
+//
+//  Created by Alexander Esplin on 3/24/11.
+//  Copyright 2011 Alex Esplin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface AETasksTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ AETasksTests/AETasksTests.m
@@ -0,0 +1,33 @@
+//
+//  AETasksTests.m
+//  AETasksTests
+//
+//  Created by Alexander Esplin on 3/24/11.
+//  Copyright 2011 Alex Esplin. All rights reserved.
+//
+
+#import "AETasksTests.h"
+
+
+@implementation AETasksTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AETasksTests");
+}
+
+@end
