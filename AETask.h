b7b375901f2e146a40d0d418776bcba3f0b4f3b5
Alex Esplin <alex.esplin@gmail.com>
2011-03-24 10:46:34 -0600

    added function stubs declarations for creation functions

--- AETask.h
+++ AETask.h
@@ -7,12 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "StandardIncludes.h"
 
 
 @interface AETask : NSObject {
     
     NSString        *TaskName;
     NSString        *Category;
+    AETState         State;
     NSMutableArray  *SubTaskList;
     NSMutableArray  *Tags;
     NSDate          *DueDate;
@@ -21,5 +23,52 @@
 @private
     
 }
+
+/*******************************************************************************
+ *                              Properties                                     *
+ ******************************************************************************/
+
+@property (readwrite,assign)    int priority;
+@property (readwrite,assign)    AETState state;
+@property (readonly)            NSString *taglist;
+
+/*******************************************************************************
+ *                          Instance Functions                                 *
+ ******************************************************************************/
+
+/* creators */
+
+- (id) initWithName:(NSString *)name;
+- (id) initWithName:(NSString *)name 
+            DueDate:(NSDate *)dueDate;
+- (id) initWithName:(NSString *)name 
+            DueDate:(NSDate *)dueDate 
+           Priority:(int)prio;
+- (id) initWithName:(NSString *)name 
+            DueDate:(NSDate *)dueDate 
+           Priority:(int)prio 
+            TagList:(NSArray *)tagList;
+
+/* modifiers */
+- (void) addSubTask:(AETask *)subTask;
+- (void) addTag:(NSString *)tag;
+
+
+/*******************************************************************************
+ *                              Class Functions                                *
+ ******************************************************************************/
+
+/* creators */
++ (id) TaskWithName:(NSString *)name;
++ (id) TaskWithName:(NSString *)name 
+            DueDate:(NSDate *)dueDate;
++ (id) TaskWithName:(NSString *)name 
+            DueDate:(NSDate *)dueDate 
+           Priority:(int)prio;
++ (id) TaskWithName:(NSString *)name 
+            DueDate:(NSDate *)dueDate 
+           Priority:(int)prio 
+            TagList:(NSArray *)tagList;
+
 
 @end
--- AETask.m
+++ AETask.m
@@ -11,6 +11,34 @@
 
 @implementation AETask
 
+
+/*******************************************************************************
+ *                              Properties                                     *
+ ******************************************************************************/
+@synthesize priority=Priority;
+@synthesize state=State;
+
+/**
+ Return a ", " delimited list of tags belonging to this task
+ 
+ @param None
+ @return NSString
+ */
+- (NSString *)taglist {
+    int tIndex;
+    
+    NSMutableString *tl = [[NSString alloc] initWithFormat:@""];
+    
+    for (tIndex = 0; tIndex < [Tags count]; tIndex++) {
+        [tl appendFormat:@"%@",[Tags objectAtIndex:tIndex]];
+        if (tIndex < [Tags count] - 1) {
+            [tl appendFormat:@", "];
+        }
+    }
+    
+    return tl;
+}
+
 - (id)init
 {
     self = [super init];
