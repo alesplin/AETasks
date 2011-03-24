2ca7b18a468e77c0ba125347b91933e81c0196ba
Alex Esplin <alex.esplin@gmail.com>
2011-03-24 08:08:17 -0600

    Added StandardIncludes.h

---
+++ AETasks.xcodeproj/StandardIncludes.h
@@ -0,0 +1,10 @@
+//
+//  StandardIncludes.h
+//  AETasks
+//
+//  Created by Alexander Esplin on 3/24/11.
+//  Copyright 2011 Alex Esplin. All rights reserved.
+//
+
+
+void QuietLog (NSString *format, ...);
