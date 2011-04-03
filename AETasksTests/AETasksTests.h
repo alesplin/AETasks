e653aa064e8fe1123f024ee68d4e19210b2313ef
Alex Esplin <alex.esplin@gmail.com>
2011-04-03 15:47:43 -0600

    added task items for testing, implemented test setup and teardown.

--- AETasksTests/AETasksTests.h
+++ AETasksTests/AETasksTests.h
@@ -10,6 +10,28 @@
 
 
 @interface AETasksTests : SenTestCase {
+    NSString    *tn1;
+    NSString    *tn2;
+    NSString    *tn3;
+    NSString    *tn4;
+    NSString    *tn5;
+    
+    NSString    *tag1;
+    NSString    *tag2;
+    NSString    *tag3;
+    NSString    *tag4;
+    NSString    *tag5;
+    
+    NSString    *tc1;
+    NSString    *tc2;
+    NSString    *tc3;
+    NSString    *tc4;
+    
+    NSDate      *nowDate;
+    NSDate      *fd1;
+    NSDate      *fd2;
+    NSDate      *fd3;
+    
 @private
     
 }
--- AETasksTests/AETasksTests.m
+++ AETasksTests/AETasksTests.m
@@ -15,12 +15,52 @@
 {
     [super setUp];
     
-    // Set-up code here.
+    tn1 = [NSString stringWithFormat:@"task1"];
+    tn2 = [NSString stringWithFormat:@"task2"];
+    tn3 = [NSString stringWithFormat:@"task3"];
+    tn4 = [NSString stringWithFormat:@"task4"];
+    tn5 = [NSString stringWithFormat:@"task5"];
+    
+    tag1 = [NSString stringWithFormat:@"tag1"];
+    tag2 = [NSString stringWithFormat:@"tag2"];
+    tag3 = [NSString stringWithFormat:@"tag3"];
+    tag4 = [NSString stringWithFormat:@"tag4"];
+    tag5 = [NSString stringWithFormat:@"tag5"];
+    
+    tc1 = [NSString stringWithFormat:@"category1"];
+    tc2 = [NSString stringWithFormat:@"category2"];
+    tc3 = [NSString stringWithFormat:@"category3"];
+    tc4 = [NSString stringWithFormat:@"category4"];
+    
+    nowDate = [NSDate date];
+    fd1 = [NSDate dateWithTimeIntervalSinceNow:3600];       /* 1 hour in future */
+    fd2 = [NSDate dateWithTimeIntervalSinceNow:3600*24];    /* 1 day in future */
+    fd3 = [NSDate dateWithTimeIntervalSinceNow:3600*18];    /* 18 hours in future */
 }
 
 - (void)tearDown
 {
-    // Tear-down code here.
+    [tn1 release];
+    [tn2 release];
+    [tn3 release];
+    [tn4 release];
+    [tn5 release];
+    
+    [tag1 release];
+    [tag2 release];
+    [tag3 release];
+    [tag4 release];
+    [tag5 release];
+    
+    [tc1 release];
+    [tc2 release];
+    [tc3 release];
+    [tc4 release];
+    
+    [nowDate release];
+    [fd1 release];
+    [fd2 release];
+    [fd3 release];
     
     [super tearDown];
 }
