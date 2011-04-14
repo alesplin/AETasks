a5b90b51fc6c0bb32c6959acecf5657e020127e8
Alex Esplin <alex.esplin@gmail.com>
2011-04-14 07:43:24 -0600

    added test for class initialization methods

--- AETasksTests/AETasksTests.m
+++ AETasksTests/AETasksTests.m
@@ -111,6 +111,68 @@
                    tc1,task6.category);
 }
 
+- (void) testClassInit {
+    task1 = [AETask taskWithName:tn1];
+    task2 = [AETask taskWithName:tn1 
+                         DueDate:nowDate];
+    task3 = [AETask taskWithName:tn1 
+                         DueDate:nowDate 
+                        Priority:100];
+    task4 = [AETask taskWithName:tn1 
+                         DueDate:nowDate 
+                        Priority:100 
+                         TagList:tl1];
+    task5 = [AETask taskWithName:tn1 
+                         DueDate:nowDate 
+                        Priority:100 
+                         TagList:tl1 
+                           State:AETStateInProgress];
+    task6 = [AETask taskWithName:tn1 
+                         DueDate:nowDate 
+                        Priority:100 
+                         TagList:tl1 
+                           State:AETStateInProgress 
+                        Category:tc1];
+    
+    STAssertEquals(task1.name, 
+                   tn1, 
+                   @"Expected: %@ Actual: %@",
+                   tn1,task1.name);
+    STAssertTrue(task1.priority == NO_PRIORITY, 
+                 @"Expected: %d Actual: %d", NO_PRIORITY,task1.priority);
+    STAssertTrue([task1.dueDate isEqualToDate:[NSDate distantFuture]], 
+                 @"Expcted: %@ Actual: %@", [NSDate distantFuture], task1.dueDate);
+    STAssertTrue((task1.tagList != nil) && ([task1.tagList count] == 0), 
+                 @"task1.tagList nil or has items...");
+    STAssertTrue(task1.state == AETStateNone, 
+                 @"Expected: %d Actual: %d", AETStateNone,task1.state);
+    STAssertTrue([task1.category isEqualToString:@"None"], 
+                 @"Expected: %@ Actual: %@", @"None",task1.category);
+    STAssertEquals(task2.dueDate, 
+                   nowDate, 
+                   @"Expected: %@ Actual: %@", 
+                   nowDate,task2.dueDate);
+    STAssertEquals(task3.priority, 
+                   100, 
+                   @"Expected: %d Actual: %d", 
+                   100,task3.priority);
+    
+    for (int i = 0; i < [task4.tagList count]; i++) {
+        NSString *actual = [task4.tagList objectAtIndex:i];
+        NSString *expected = [tl1 objectAtIndex:i];
+        STAssertEquals(actual, expected, @"Expected: %@ Actual: %@", expected,actual);
+    }
+    
+    STAssertEquals(task5.state, 
+                   AETStateInProgress, 
+                   @"Expected: %d Actual: %d", 
+                   AETStateInProgress,task5.state);
+    STAssertEquals(task6.category, 
+                   tc1, 
+                   @"Expected:%@ Actual: %@", 
+                   tc1,task6.category);
+}
+
 @end
 
 
